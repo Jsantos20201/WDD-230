d797d1d88367d328889b22b1475de16774ecfa09
Jsantos20201 <72623931+Jsantos20201@users.noreply.github.com>
2022-04-20 12:18:03 -0600

    changes in code

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,15 @@
 let date = new Date();
 console.log(date); 
+
+let year = date.getFullYear();
+console.log(year);
+
+document.querySelector('h1').textContent = year; //query selector is to select an element in html file.
+
+document.querySelector("div").innerHTML = "<h2>Hello world!</h2>";
+
+
+// how to add an element to html file through java script.
+let newpara = document.createElement("p");
+newpara.textContent = 'This is my paragraph';
+document.querySelector("div").appendChild(newpara);
