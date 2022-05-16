f6374bb4d13852867df3335922eeefc95267f574
Jsantos20201 <san18036@byui.edu>
2022-05-16 17:35:19 -0600

    chnages

--- Chamber/js/main.js
+++ Chamber/js/main.js
@@ -1,4 +1,15 @@
-let hambuttom = document.querySelector(".menu");
-let mainnav = document.querySelector("nav");
-hambuttom.addEventListener("click", () => {mainnav.classList.toggle("active")});
+function hamBurgerandTime() {
+    let hambuttom = document.querySelector(".menu");
+    let mainnav = document.querySelector("nav");
+    hambuttom.addEventListener("click", () => {mainnav.classList.toggle("active")});
 
+
+    const datafield = document.querySelector(".date");
+    const date = new Date();
+    console.log(date);
+    const fulldate = new Intl.DateTimeFormat("en-UK", { dateStyle: "full" }).format(date);
+    document.write(fulldate);
+    document.querySelector("span").innerHTML = fulldate;
+}
+
+hamBurgerandTime();
