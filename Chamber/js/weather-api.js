91bcb0bc7a420c7c676bf05ab1a68e99164f0b11
Jsantos20201 <san18036@byui.edu>
2022-06-24 19:13:26 -0600

    changes

---
+++ Chamber/js/weather-api.js
@@ -0,0 +1,18 @@
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5223358&appid=100b258478d087b7a4df4424ee2be815";
+const getWeather = async () => {
+    const response = await fetch(apiURL);
+    jsObject = await response.json();
+    console.log(jsObject);
+    let f = (jsObject.main.temp - 273.15) * 1.8 + 32;
+    document.querySelector('#current-temp').textContent = f.toFixed(0);
+
+    const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    const desc = jsObject.weather[0].description;
+    document.querySelector('#icon-src').textContent = iconsrc;
+    document.querySelector('#weathericon').setAttribute('src', iconsrc);
+    document.querySelector('#weathericon').setAttribute('alt', desc);
+    document.querySelector('figcaption').textContent = desc; 
+    document.querySelector("#wind-speed").textContent = "Wind Speed: " + jsObject.wind.speed;
+    document.querySelector("#wind-chill").textContent = "Wind Chill: " + jsObject.wind.gust;   
+  };
+getWeather();
