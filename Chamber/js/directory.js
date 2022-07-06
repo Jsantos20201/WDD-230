d355e659747c263addf117ddd3918bdc50226f1b
Jsantos20201 <san18036@byui.edu>
2022-07-05 18:32:31 -0600

    changes

--- Chamber/js/directory.js
+++ Chamber/js/directory.js
@@ -1,17 +1,17 @@
 const requestURL = 'https://raw.githubusercontent.com/Jsantos20201/WDD-230/master/Chamber/json/data.json';
 
-let btn1 = document.querySelector('.btn1');
-let btn2 = document.querySelector('.btn2');
+let btn1 = document.querySelector(".btn1");
+let btn2 = document.querySelector(".btn2");
 
 fetch(requestURL)
   .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
-    const businesses = jsonObject['businesses'];
+    const businesses = jsonObject["businesses"];
     
-    var table = document.querySelector('.table');
-    var cards = document.querySelector('.cards');
+    var table = document.querySelector(".table");
+    var cards = document.querySelector(".cards");
 
     businesses.forEach(displayBusinesses);
     businesses.forEach(displayTable);
@@ -29,33 +29,33 @@ fetch(requestURL)
 
 
 
-const mediaQuery = window.matchMedia('(max-width: 900px)')
+const mediaQuery = window.matchMedia("(max-width: 900px)")
 if (mediaQuery.matches) {
-  cards.style.display = 'none';
-  table.style.display = 'table';
+  cards.style.display = "none";
+  table.style.display = "table";
 }
 
-const mediaQuery3 = window.matchMedia('(max-width: 690px)')
+const mediaQuery3 = window.matchMedia("(max-width: 690px)")
 if (mediaQuery3.matches) {
-  cards.style.display = 'flex';
-  table.style.display = 'none';
+  cards.style.display = "flex";
+  table.style.display = "none";
 
 }
 
-const mediaQuery2 = window.matchMedia('(max-width: 690px)')
+const mediaQuery2 = window.matchMedia("(max-width: 690px)")
 if (mediaQuery2.matches) {
-  let addrList = document.querySelectorAll('.add');
+  let addrList = document.querySelectorAll(".add");
   for (let i = 0; i < addrList.length; i++) {
-    addrList[i].style.display = 'none';
+    addrList[i].style.display = "none";
   }
 
 }
 
-const mediaQuery4 = window.matchMedia('(min-width: 700px)')
+const mediaQuery4 = window.matchMedia("(min-width: 700px)")
 if (mediaQuery4.matches) {
-  let addrList = document.querySelectorAll('.add');
+  let addrList = document.querySelectorAll(".add");
   for (let i = 0; i < addrList.length; i++) {
-    addrList[i].style.display = '';
+    addrList[i].style.display = "";
   }
 }
 
@@ -64,52 +64,52 @@ if (mediaQuery4.matches) {
 
 
   function displayBusinesses(business) {  
-    let card = document.createElement('section');
-    let div = document.createElement('div');
-    let img = document.createElement('img');
-    img.setAttribute('src', business.imageurl);
-    img.setAttribute('alt', `${business.name}'s logo`);
+    let card = document.createElement("section");
+    let div = document.createElement("div");
+    let img = document.createElement("img");
+    img.setAttribute("src", business.imageurl);
+    img.setAttribute("alt", `${business.name}'s logo`);
     div.appendChild(img);
     card.appendChild(div);
 
-    let h2 = document.createElement('h2');  
+    let h2 = document.createElement("h2");  
     h2.textContent = business.name;
    
     card.appendChild(h2);
 
-    let address = document.createElement('p');
+    let address = document.createElement("p");
     address.textContent = business.address;
     card.appendChild(address);
 
 
-    let phone = document.createElement('p');
+    let phone = document.createElement("p");
     phone.textContent = business.phone;
     card.appendChild(phone);
 
-    let website = document.createElement('p');
+    let website = document.createElement("p");
     website.textContent = business.website;
-    website.setAttribute('class', 'web');
+    website.setAttribute("class", "web");
     card.appendChild(website);
 
 
     
 
-    document.querySelector('div.cards').appendChild(card);
+    document.querySelector("div.cards").appendChild(card);
   }
 
   function displayTable(business){
-    let list_row = document.createElement('tr');
-    let list_td = document.createElement('td');
+    let list_row = document.createElement("tr");
+    let list_td = document.createElement("td");
     list_td.textContent = business.name;
 
-    let td_add = document.createElement('td');
+    let td_add = document.createElement("td");
     td_add.textContent = business.address;
-    td_add.setAttribute('class', 'add');
+    td_add.setAttribute("class", "add");
 
-    let td_cell = document.createElement('td');
+    let td_cell = document.createElement("td");
     td_cell.textContent = business.phone;
 
-    let td_web = document.createElement('td');
+    let td_web = document.createElement("td");
     td_web.textContent = business.website;
 
     list_row.appendChild(list_td);
@@ -117,6 +117,6 @@ if (mediaQuery4.matches) {
     list_row.appendChild(td_add);
     list_row.appendChild(td_cell);
 
-    document.querySelector('.table').appendChild(list_row);
+    document.querySelector(".table").appendChild(list_row);
 
   }
--- Chamber/js/main.js
+++ Chamber/js/main.js
@@ -15,6 +15,12 @@ function hamBurgerandTime() {
     const fulldate = new Intl.DateTimeFormat("en-UK", { dateStyle: "full" }).format(date);
     document.write(fulldate);
     document.querySelector("span").innerHTML = fulldate;
+
+    if (date.getDay() == 1 || date.getDay() == 2){
+        const banner = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+        document.querySelector(".banner").textContent = banner;
+        document.querySelector(".banner").style.cssText = "padding: 5px;";
+    }
 }
 
 hamBurgerandTime();
