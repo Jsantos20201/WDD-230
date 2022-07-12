9b348836b63596e6e72b70a23d16afbd01df1711
Jsantos20201 <san18036@byui.edu>
2022-07-12 09:23:30 -0600

    chnages

--- Final project/scripts/main.js
+++ Final project/scripts/main.js
@@ -1,8 +1,19 @@
-function hamBurgerandTime() {
+function main() {
+    //Hamburger menu
     let hambuttom = document.querySelector(".menu");
     let mainnav = document.querySelector("nav");
     hambuttom.addEventListener("click", () => {mainnav.classList.toggle("active")});
 
+    //tells date
+    let date = new Date();
+    let year = date.getFullYear();
+    document.getElementById("year").innerHTML = "&copy" + year + " | Joseph K. Santos | Idaho";
+    
+
+    //This part of the function is the updated date.
+    let currentdate = document.lastModified;
+    document.write(currentdate);
+    document.getElementById("updated-date").innerHTML = "Last Updated: " + currentdate;
 }
 
-hamBurgerandTime();
+main();
