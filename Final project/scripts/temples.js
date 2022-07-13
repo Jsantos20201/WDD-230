09aeb07b877a4cc4674b819d73e4b1b9dc38577e
Jsantos20201 <san18036@byui.edu>
2022-07-13 16:14:26 -0600

    changes

--- Final project/scripts/temples.js
+++ Final project/scripts/temples.js
@@ -10,19 +10,22 @@ fetch(requestURL)
     var cards = document.querySelector(".cards");
 
     temples.forEach(displayTemples);
+    temples.forEach(listenForLikes);
     
   });
-
-  displayTemples();
 
   function displayTemples(temple) {  
     let card = document.createElement("section");
     let div = document.createElement("div");
     let img = document.createElement("img");
+    let like = document.createElement("div");
+    
     img.setAttribute("src", temple.imgurl);
     img.setAttribute("alt", `${temple.title}`);
+    like.classList.add("like-yes", "like-no");
     div.appendChild(img);
     card.appendChild(div);
+    card.appendChild(like);
 
     let h2 = document.createElement("h2");  
     h2.textContent = temple.title;
@@ -38,5 +41,38 @@ fetch(requestURL)
     phone.textContent = temple.telephone;
     card.appendChild(phone);
 
+    let service = document.createElement("p");
+    service.textContent = temple.services;
+    card.appendChild(service);
+
+    let history = document.createElement("p");
+    history.textContent = temple.history;
+    card.appendChild(history);
+
+    let ordin = document.createElement("p");
+    ordin.textContent = temple.Ordinance;
+    card.appendChild(ordin);
+
+    let closure = document.createElement("p");
+    closure.textContent = temple.closure;
+    card.appendChild(closure);
+
     document.querySelector("div.cards").appendChild(card);
   }
+
+  const listenForLikes = () => {
+    const likes = document.querySelectorAll(".like");
+    likes.forEach(like => {
+     like.addEventListener("click", (event) => {
+       event.target.classList.toggle("like-no");
+       event.target.classList.toggle("like-yes");
+       if (event.target.classList.contains("like-yes")) {
+         console.log("Saving Favorite...");
+         getFaveData(event.target);
+       } else {
+         console.log("Removing Favorite...");
+         getFaveData(event.target);
+       }
+     })
+    })
+  } 
