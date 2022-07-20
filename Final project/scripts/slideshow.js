c7a5911631219703f8744497bb611beb09d10236
Jsantos20201 <san18036@byui.edu>
2022-07-19 18:07:55 -0600

    CHNAGES

--- Final project/scripts/slideshow.js
+++ Final project/scripts/slideshow.js
@@ -1,4 +1,5 @@
 let slideIndex = 0;
+let slideIndex2 = 0;
 showSlides();
 
 function showSlides() {
@@ -10,5 +11,5 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 7000); // Change image every 2 seconds
+  setTimeout(showSlides, 7000); 
 }
